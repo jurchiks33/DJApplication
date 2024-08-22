7f4fc9030226306e8e9a15703dfcc14e0ea85fde
jurchiks33 <jurisslisans@gmail.com>
2024-08-22 11:42:18 +0200

    inroducing waveform

---
+++ WaveformDisplay.h
@@ -0,0 +1,29 @@
+/*
+  ==============================================================================
+
+    WaveformDisplay.h
+    Created: 22 Aug 2024 11:38:46am
+    Author:  Juris Slisans
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+
+//==============================================================================
+/*
+*/
+class WaveformDisplay  : public juce::Component
+{
+public:
+    WaveformDisplay();
+    ~WaveformDisplay() override;
+
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (WaveformDisplay)
+};
