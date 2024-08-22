f6ef28b4b23e94bf1ac1e1821bc662d3ddc73865
jurchiks33 <jurisslisans@gmail.com>
2024-08-22 10:01:55 +0200

    drag and drop implementation

--- DeckGUI.h
+++ DeckGUI.h
@@ -10,7 +10,8 @@
 */
 class DeckGUI  : public juce::Component,
                  public juce::Button::Listener,
-                 public juce::Slider::Listener
+                 public juce::Slider::Listener,
+                 public juce::FileDragAndDropTarget
 {
 public:
     DeckGUI(DJAudioPlayer* player);
@@ -24,6 +25,9 @@ public:
     
     /**Implement Slider::Listener**/
     void sliderValueChanged(juce::Slider *slider) override;
+    
+    bool isInterestedInFileDrag(const juce::StringArray &files) override;
+    void filesDropped (const juce::StringArray &files, int x, int y) override;
 
 private:
     
