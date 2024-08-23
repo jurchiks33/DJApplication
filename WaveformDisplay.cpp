1561a656faa99e843ae36328862282546f6cdaf2
jurchiks33 <jurisslisans@gmail.com>
2024-08-23 20:53:12 +0200

    wave url

--- WaveformDisplay.cpp
+++ WaveformDisplay.cpp
@@ -51,3 +51,8 @@ void WaveformDisplay::resized()
     // components that your component contains..
 
 }
+
+void WaveformDisplay::loadURL(juce::URL audioURL)
+{
+    std::cout << "wfd:loadURL" << std::endl;
+}
