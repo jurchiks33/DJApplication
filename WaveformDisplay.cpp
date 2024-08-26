aa771e8e2c8eda750bc90b67d5d273d66e5fcc38
jurchiks33 <jurisslisans@gmail.com>
2024-08-26 12:17:47 +0200

    playhead

--- WaveformDisplay.cpp
+++ WaveformDisplay.cpp
@@ -14,8 +14,9 @@
 //==============================================================================
 WaveformDisplay::WaveformDisplay(juce::AudioFormatManager & formatManagerToUse,
                                  juce::AudioThumbnailCache & cacheToUse) :
-audioThumb(1000, formatManagerToUse, cacheToUse),
-fileLoaded(false)
+                                 audioThumb(1000, formatManagerToUse, cacheToUse),
+                                 fileLoaded(false),
+                                 position(0)
 {
     // In your constructor, you should add any child components, and
     // initialise any special settings that your component needs.
@@ -51,6 +52,8 @@ void WaveformDisplay::paint (juce::Graphics& g)
                                0,
                                1.0f
                                );
+        g.setColour(juce::Colours::lightgreen);
+        g.drawRect(position * getWidth(), 0, getWidth() / 20, getHeight());
     }
     else 
     {
@@ -91,5 +94,10 @@ void WaveformDisplay::changeListenerCallback (juce::ChangeBroadcaster *source)
 
 void WaveformDisplay::setPositionRelative(double pos)
 {
+    if (pos != position)
+    {
+        position = pos;
+        repaint();
+    }
     
 }
