570d078767a65b2db48ebc8e4bc50b16203272fe
jurchiks33 <jurisslisans@gmail.com>
2024-08-27 13:19:12 +0200

    playlist component, set bounds

--- MainComponent.cpp
+++ MainComponent.cpp
@@ -73,8 +73,10 @@ void MainComponent::paint (juce::Graphics& g)
 
 void MainComponent::resized()
 {
-    deckGUI1.setBounds(0, 0, getWidth()/2, getHeight());
-    deckGUI2.setBounds(getWidth()/2, 0, getWidth()/2, getHeight());
+    deckGUI1.setBounds(0, 0, getWidth()/2, getHeight() / 2);
+    deckGUI2.setBounds(getWidth()/2, 0, getWidth()/2, getHeight() / 2);
+    
+    playlistComponent.setBounds(0, getHeight() / 2, getWidth(), getHeight() / 2);
 }
 
 
