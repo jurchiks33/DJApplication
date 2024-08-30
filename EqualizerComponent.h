64dd389a5044ed128ad914d6469feb1a2f2204b4
jurchiks33 <jurisslisans@gmail.com>
2024-08-30 11:05:17 +0200

    slider listeners

--- EqualizerComponent.h
+++ EqualizerComponent.h
@@ -14,7 +14,8 @@
 //==============================================================================
 /*
 */
-class EqualizerComponent  : public juce::Component
+class EqualizerComponent  : public juce::Component,
+                            public juce::Slider::Listener
 {
 public:
     EqualizerComponent();
@@ -22,7 +23,13 @@ public:
 
     void paint (juce::Graphics&) override;
     void resized() override;
+    
+    /**Slider::Listener**/
+    void sliderValueChanged (juce::Slider* slider) override;
 
 private:
+    juce::Slider lowSlider;     //low frequency slider
+    juce::Slider midSlider;     // middle frequency slider
+    juce::Slider highSlider;    // high frequency slider
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (EqualizerComponent)
 };
